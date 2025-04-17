54d7473d743509c256bf90f3676b266fec18c413
jey <code@jnf.dev>
2025-04-16 21:14:13 -0700

    entrypoint for all this, the thing we'll eventually invoke on a schedule/automatically

---
+++ bandy.rb
@@ -0,0 +1,28 @@
+# dependencies
+require 'dotenv/load'
+require 'date'
+require 'logger'
+require 'httparty'
+require 'nokogiri'
+require 'json'
+require 'pstore' # https://ruby-doc.org/3.4.1/stdlibs/pstore/PStore.html
+require 'pry'
+
+# api classes
+require_relative './api/baseapi.rb'
+
+# core scripts
+Dir[File.join(__dir__, 'scripts', '*.rb')].each { |file| require file }
+
+# add any new collection items to the db
+CollectionSync.new(debug: true).run
+
+# enrich any 'seen' items
+EnrichItems.new(debug: true).run
+
+# check for new downloadables 
+# could be either new collection items or pre-releases being released
+
+# download new downloadables
+
+# unpack new unpackables
